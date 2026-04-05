30b406d806d6f0d6956e1ab9c15d67e3741717e9
Tiago Matos <156955506+TiagoMa7os@users.noreply.github.com>
2026-04-05 13:18:23 +0000

    melhoria no admin com list_display, search_fields, list_filter e filter_horizontal

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Licenciatura)
+class LicenciaturaAdmin(admin.ModelAdmin):
+    list_display = ("nome", "sigla", "semestres", "creditos")
+    search_fields = ("nome",)
+
+
+@admin.register(UnidadeCurricular)
+class UnidadeCurricularAdmin(admin.ModelAdmin):
+    list_display = ("nome", "ano", "semestre", "ects")
+    search_fields = ("nome",)
+    list_filter = ("ano", "semestre")
+
+
+@admin.register(Projeto)
+class ProjetoAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "data", "unidade_curricular")
+    search_fields = ("titulo",)
+    list_filter = ("data",)
+    filter_horizontal = ("tecnologias", "competencias", "tags")
+
+
+@admin.register(Tecnologia)
+class TecnologiaAdmin(admin.ModelAdmin):
+    list_display = ("nome", "tipo", "interesse")
+    search_fields = ("nome",)
+
+
+@admin.register(Competencia)
+class CompetenciaAdmin(admin.ModelAdmin):
+    list_display = ("nome", "nivel")
+    search_fields = ("nome",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
+
+
+@admin.register(Formacao)
+class FormacaoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "entidade", "data_inicio")
+    search_fields = ("nome",)
+
+
+@admin.register(TFC)
+class TFCAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "ano", "interesse")
+    search_fields = ("titulo",)
+
+
+@admin.register(MakingOf)
+class MakingOfAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "data")
+    search_fields = ("titulo",)
